1d6ca85dfe10b712cd5a2899a9920f9139f6ab9f
Juan Jose Duque Cardona <jjduquec@unal.edu.co>
2021-11-04 15:39:14 -0500

    modificado ejercicio de notificacion simple

--- kotlin/notificacionsimple/app/src/main/java/com/example/notificacionsimple/MainActivity.kt
+++ kotlin/notificacionsimple/app/src/main/java/com/example/notificacionsimple/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.notificacionsimple
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -10,17 +12,50 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val numeroAleatorio=(Math.random() * 100_001).toInt()
+
+        //declaracion de variables
+        val preferencias=getSharedPreferences("puntos",Context.MODE_PRIVATE);
+        //puntos por defecto
+        var editor=preferencias.edit();
+        editor.putInt("puntuacion",0);
+        editor.commit()
+        var puntuacion_actual=preferencias.getInt("puntuacion",0);
+        //programamos los puntos aleatorios
+        var numeroAleatorio=(Math.random() * 100_001).toInt()
         Toast.makeText(this, "Memoriza este numero: ${numeroAleatorio}", Toast.LENGTH_LONG).show()
+
+
+
+
         bt.setOnClickListener {
             //obtenemos el numero
             if(numeroAleatorio==numero.text.toString().toInt()){
                 Toast.makeText(this, "CORRECTO!", Toast.LENGTH_LONG).show()
 
+                puntuacion_actual+=5
+                puntuacion_vw.setText("Puntuacion:${puntuacion_actual}")
+                editor.putInt("puntuacion",puntuacion_actual)
+                editor.commit()
+                numero.setText("");
+
             }else{
 
                 Toast.makeText(this, "Ups! Te equivocaste", Toast.LENGTH_LONG).show()
+                puntuacion_actual-=5
+                puntuacion_vw.setText("Puntuacion:${puntuacion_actual}")
+                editor.putInt("puntuacion",puntuacion_actual)
+                editor.commit()
+                numero.setText("");
             }
+
+        }
+
+
+        bt_jugar.setOnClickListener {
+            numeroAleatorio=(Math.random() * 100_001).toInt()
+            Toast.makeText(this, "Memoriza este numero: ${numeroAleatorio}", Toast.LENGTH_LONG).show()
+            numero.setText("");
+
 
         }
 
